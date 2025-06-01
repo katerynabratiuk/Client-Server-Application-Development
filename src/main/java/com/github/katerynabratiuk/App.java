11574c9598f57b4dc04d4cd693fba68ba5ed0921
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-01 16:34:14 +0300

    Add App class

---
+++ src/main/java/com/github/katerynabratiuk/App.java
@@ -0,0 +1,18 @@
+package com.github.katerynabratiuk;
+
+import com.github.katerynabratiuk.securePacket.Message;
+import com.github.katerynabratiuk.securePacket.Packet;
+import com.github.katerynabratiuk.securePacket.PacketDecoder;
+import com.github.katerynabratiuk.securePacket.PacketEncoder;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+public class App
+{
+    public static void main( String[] args ) {
+
+    }
+}
